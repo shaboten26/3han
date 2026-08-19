27582d8f124a1cec6561d43fd96e78cc3e459f78
shaboten26 <237569585+shaboten26@users.noreply.github.com>
2026-08-20 08:43:22 +0900

    add getChartData, 細かい修正

--- script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("button");
-function push(){
-    button.style.backgroundColor = "red";
-}
-
-button.addEventListener("click", push);
--- top_script.js
+++ top_script.js
@@ -1,6 +1,14 @@
+import { startOfMonth } from "https://esm.run";
 import { db } from "./db";
 
+const chartElement = document.getElementById("chart");
 
-const _getChartData = async () => {
-    await db.money.
-}
+const getChartData = async () => {
+	const result = await db.money
+		.where("date")
+		.above(startOfMonth(new Date()))
+		.toArray();
+	return result;
+};
+
+chartElement.addEventListener("click", getChartData);
