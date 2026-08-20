f7eab9d9d1db1ec6a684f77976b218e4c0cbdd7e
yokoyama_tat <tat_57_tat@au.com>
2026-08-20 17:35:14 +0900

    あと日付をタイムスタンプから直せない

--- top_script.js
+++ top_script.js
@@ -40,21 +40,42 @@ document.addEventListener("DOMContentLoaded", async () => {
 //直近の履歴表示
 document.getElementById("recent").addEventListener("click", async () => {updateRecentHistory()});
 
-function updateRecentHistory() {
+async function updateRecentHistory() {
 	const recentHistory = document.getElementById("recent-history");
-	const transactions = JSON.parse(localStorage.getItem('transactions')) || [];//jsonがうまく読み取れない?
 	const recentLength = recentHistory.childElementCount;
-	for (let i = 0; i < 2; i++) {
-		if (recentLength + i < transactions.length) {
-			const transaction = transactions[transactions.length - 1 - i - recentLength];
+	console.log(recentLength);
+	const recentData = await db.money.orderBy("date").reverse().offset(recentLength).limit(2).toArray();
+    console.log("recentData");
+    console.log(recentData);
+	recentData.forEach(transaction =>{
+		    console.log(transaction);
 			const card = document.createElement("div");
 			card.classList.add("card");
-			card.innerHTML = `
-				<p>${transaction.amount}円</p>
-			`;
+			let date = new Date(transaction.date * 1000);
+			if(transaction.price > 0){
+				card.innerHTML = `
+					<h1 class="income">+${transaction.price}円</h1>
+					<p>${date.toLocaleDateString()}</p>
+				`;
+			}else{
+				card.innerHTML = `
+					<h1 class="expense">${transaction.price}円</h1>
+					<p>${date.toLocaleDateString().slice(5)}</p>
+					<p>${transaction.purpose}</p>
+					<p>${transaction.place}</p>
+				`;
+			}
 			recentHistory.appendChild(card);
-		    console.log(transaction.amount);
+
+	});
+	if(recentData.length === 0){
+		if(recentLength % 2 == 1){
+			recentHistory.appendChild(document.createElement("div"));
 		}
+		const noMoreData = document.createElement("p");
+		noMoreData.textContent = "これ以上履歴はありません。";
+		recentHistory.appendChild(noMoreData);
+		document.getElementById("recent").remove();
 	}
 };
 
